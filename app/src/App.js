41d5dfdb0d747354290a7153df331a57ce231429
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2020-03-08 20:47:46 -0400

    app-rendering

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Quotes from './components/Quotes'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Programming Quotes</h1>
+      <Quotes />
+    </div>
+  );
+}
+
+export default App;
